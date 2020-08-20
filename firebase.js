f0ba45bc37fe0fdfc49706086a2f2ce816157571
Gerardo Mayboca <59183708+Guerrimau@users.noreply.github.com>
2020-08-19 19:38:33 -0700

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAgDq-FzlFV_nKZ39SF26j0l2Rz54bZ7Wc",
-    authDomain: "journal-app-react-ab707.firebaseapp.com",
-    databaseURL: "https://journal-app-react-ab707.firebaseio.com",
-    projectId: "journal-app-react-ab707",
-    storageBucket: "journal-app-react-ab707.appspot.com",
-    messagingSenderId: "1068304548606",
-    appId: "1:1068304548606:web:f7c6be2f0fd2d89a469baf"
-};
